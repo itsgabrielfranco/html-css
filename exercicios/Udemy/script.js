7505bb20d8699e515bb40cba25c58e625abfd443
itsgabrielfranco <gabrielaugustofa21@gmail.com>
2024-01-04 20:25:42 -0300

    updatejs002
    updatejs

--- exercicios/Udemy/script.js
+++ exercicios/Udemy/script.js
@@ -3,12 +3,19 @@ ario
 */
 var a= 2;
 var b = 3;
+var c ="nada";
 
 var soma = a+b;
 var sub = a-b;
 var mult = a*b;
 var div = a/b;
 
-if (b%2 == 1){
+if (c%2 == 1){
     alert("Número impar");
+}
+else if (c%2 == 0){
+    alert("numero par");
+}
+else {
+    alert("Número par");
 }
