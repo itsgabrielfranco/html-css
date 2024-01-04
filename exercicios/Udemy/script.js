4ba12d6fe7cb283eeb0afe4a45d76704b51ef495
itsgabrielfranco <gabrielaugustofa21@gmail.com>
2024-01-04 20:21:20 -0300

    un
    un

--- exercicios/Udemy/script.js
+++ exercicios/Udemy/script.js
@@ -1,4 +1,15 @@
 /*comentario
 ario
 */
-alert("ola mundo");
+var a= 2;
+var b = 3;
+
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
